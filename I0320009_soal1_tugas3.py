156539ec637e6cb8df5e3116503a3c6104cf7068
Angela R <angela.mutiaraputi@gmail.com>
2021-03-18 23:15:11 +0700

    Pengerjaan soal

--- I0320009_soal1_tugas3.py
+++ I0320009_soal1_tugas3.py
@@ -1,20 +1,23 @@
 #Menampilkan list 10 teman
-list = ['Adrian', 'Audrey', 'Evelyn', 'Candrika', 'Funny', 'Aratia', 'Divana', 'Ayu', 'Gea', 'Hana']
+Nama_teman = ['Adrian', 'Audrey', 'Evelyn', 'Candrika', 'Funny', 'Aratia', 'Divana', 'Ayu', 'Gea', 'Hana']
 
 #Menampilkan isi list indeks nomor 4,6,7
-print("List indeks nomor 4,6,7 yaitu", list[4], ",", list[6], ", dan", list[7])
+print("List indeks nomor 4,6,7 yaitu", Nama_teman[4], ",", Nama_teman[6], ", dan", Nama_teman[7])
 
 #Mengganti nama teman di list 3,5,9
-list[3] = 'Elisa'
-list[5] = 'Bagus'
-list[9] = 'Dendy'
+Nama_teman[3] = 'Elisa'
+Nama_teman[5] = 'Bagus'
+Nama_teman[9] = 'Dendy'
 
 #Menambahkan 2 nama teman
-list.append('Afiq')
-list.append('Fadhila')
+Nama_teman.append('Afiq')
+Nama_teman.append('Fadhila')
 
 #Menampilkan semua nama teman dengan pengulangan
-print(list*2)
+friend = 0
+for teman in range (0,12) :
+    print (Nama_teman[friend])
+    friend+=1
 
 #Menampilkan panjang list
-print(len(list))
+print("Panjang indeks list yaitu", len(Nama_teman))
--- I0320009_soal2_tugas3.py
+++ I0320009_soal2_tugas3.py
@@ -1,4 +1,4 @@
-# Membuat program dictionary
+#Membuat program dictionary
 dict = {'Nama':'Angela','Hobi1':'Menari','Hobi2':'Mendengarkan lagu','Hobi3':'Menonton youtube','Sosmed1':'line: angelarms','Sosmed2':'ig:angela.rms','Sosmed3':'twitter:geminsme','Lagu1':'My Everything','Lagu2':'Kidult','Lagu3':'Crossroads','Makanan1':'Fettucine', 'Makanan2':'Burger','Makanan3':'Nasi goreng'}
 
 #Mengubah salah satu hobi dan sosmed, menghapus 2 makanan, dan menambah 1 hobi
